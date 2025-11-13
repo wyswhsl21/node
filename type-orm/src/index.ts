ae874d5b655d518dad553ca91a21e0ba87e8dd1e
김재우 <wyswhsl21@naver.com>
2025-11-13 22:01:52 +0900

    type orm 스터디

--- type-orm/src/index.ts
+++ type-orm/src/index.ts
@@ -3,6 +3,7 @@ import express from 'express';
 //로그를 위한 미들웨어
 import morgan from 'morgan';
 import { AppDataSource } from './data-source';
+import { User } from './entity/User';
 
 const app = express();
 
@@ -16,6 +17,45 @@ app.listen(PORT, () => {
 
 app.get('/', (req, res) => {
   res.send('running...');
+});
+
+//회원가입
+app.post('/users', async (req, res) => {
+  const user = await AppDataSource.getRepository(User).create(req.body);
+  console.log(user);
+  const result = await AppDataSource.getRepository(User).save(user);
+  return res.send(result);
+});
+
+app.get('/users', async (req, res) => {
+  const result = await AppDataSource.getRepository(User).find();
+  res.json(result);
+});
+
+app.get('/users/:id', async (req, res) => {
+  const result = await AppDataSource.getRepository(User).findOneBy({
+    id: Number(req.params.id),
+  });
+  res.json(result);
+});
+
+app.put('/users/:id', async (req, res) => {
+  const user = await AppDataSource.getRepository(User).findOneBy({
+    id: Number(req.params.id),
+  });
+  if (!user) {
+    return res.status(404).json({ message: 'User not found' });
+  }
+  AppDataSource.getRepository(User).merge(user, req.body);
+  const result = await AppDataSource.getRepository(User).save(user);
+  return res.send(result);
+});
+
+app.delete('/users/:id', async (req, res) => {
+  const result = await AppDataSource.getRepository(User).delete(
+    Number(req.params.id)
+  );
+  return res.json(result);
 });
 
 AppDataSource.initialize()

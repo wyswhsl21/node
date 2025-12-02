6ff1abfdefbe522eebadfc02a0b428d800160617
김재우 <wyswhsl21@naver.com>
2025-12-02 23:18:28 +0900

    websocket

---
+++ simple-websocket/server/index.js
@@ -0,0 +1,10 @@
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({ port: 7071 });
+
+wss.on('connection', (ws) => {
+  ws.send('connected');
+
+  ws.on('message', (messageFromClient) => {
+    const message = JSON.parse(messageFromClient);
+  });
+});
